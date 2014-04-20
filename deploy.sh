5fc4f310eda4a882a56c641c70cc35bf155468cd
Hemanta Sapkota <hemanta@lumanetworks.com>
2014-04-20 13:52:57 +1000

    rebuilding site Sun 20 Apr 2014 13:52:57 EST

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,5 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:hemantasapkota/hemantasapkota.github.io master
+# git subtree push --prefix=public git@github.com:hemantasapkota/hemantasapkota.github.io master
+git subtree push --prefix=public https://github.com/hemantasapkota/hemantasapkota.github.io master
